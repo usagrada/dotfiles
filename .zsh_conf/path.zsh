2ab95d06f3ef3e765eaa5d92afb49e3f9d6f2996
usagrada <usagrada.kind@gmail.com>
2022-03-18 01:42:48 +0900

    export path in asdf bin

--- .zsh_conf/path.zsh
+++ .zsh_conf/path.zsh
@@ -2,16 +2,16 @@
 export GOENV_DISABLE_GOPATH=1
 export GOPATH="$HOME/go"
 export PATH="$HOME/go/bin:$PATH"
-# flutter
-export PATH="$HOME/development/flutter/bin:$PATH"
 
 # c++
-#export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
-#export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
 # clang
-#export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 # yarn
 export PATH="$HOME/.yarn/bin:$PATH"
+export PATH="$HOME/.asdf/shims:$PATH"
+export PATH="$(find ~/.asdf -name bin | xargs | sed -e 's/ /:/g'):$PATH"
 
 typeset -U path PATH
 path=(
--- .zshrc
+++ .zshrc
@@ -1,7 +1,7 @@
 zsh_conf=~/.zsh_conf
 
-. $zsh_conf/env.zsh
 . $zsh_conf/path.zsh
+. $zsh_conf/env.zsh
 . $zsh_conf/zinit.zsh
 . $zsh_conf/eval.zsh
 . $zsh_conf/alias.zsh
