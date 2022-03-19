3a45b54bea2f775d29eb6077b686fd952a7ad10c
usagrada <usagrada.kind@gmail.com>
2022-03-20 00:55:00 +0900

    update 0320

--- .zsh_conf/path.zsh
+++ .zsh_conf/path.zsh
@@ -7,7 +7,7 @@ export PATH="$HOME/go/bin:$PATH"
 export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
 # clang
-export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+# export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 # yarn
 export PATH="$HOME/.yarn/bin:$PATH"
 export PATH="$HOME/.asdf/shims:$PATH"
